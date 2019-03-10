56caad7c0fd6e88faf5cbed1ae9ae8ea2b4f4d31
enabokov <edwardnabokov@gmail.com>
2019-03-10 18:42:08 +0200

    Add more check functions

--- lexis/check.go
+++ lexis/check.go
@@ -1,7 +1,9 @@
 package lexis
 
 import (
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 func checkValueInArray(value string, array []string) bool {
@@ -23,9 +25,39 @@ func checkValueIsString(value string) bool {
 }
 
 func checkValueIsNumber(value string) bool {
-	if !checkValueIsString(value) {
-		return true
+	if checkValueIsString(value) {
+		return false
 	}
 
-	return false
+	if _, err := strconv.Atoi(string(value)); err != nil {
+		return false
+	}
+
+	return true
+}
+
+func checkValueIsLetter(value string) bool {
+	for _, r := range value {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func checkValueIsVariable(value string) bool {
+	if checkValueIsString(value) {
+		return false
+	}
+
+	if _, err := strconv.Atoi(string(value)); err == nil {
+		return false
+	}
+
+	if !checkValueIsLetter(value) {
+		return false
+	}
+
+	return true
 }
