fa30a40c3c6ba53f4ab8bfcf2648526148425d99
enabokov <edwardnabokov@gmail.com>
2019-04-01 14:14:02 +0300

    update test with condition

--- examples/test.go
+++ examples/test.go
@@ -5,16 +5,20 @@ import "bla"
 import "test"
 
 
-def nameFunction(a, b, c, d, f, s) {
+def nameFunction(a, b) {
 	# this is just a comment
 	var a float
 	bla.Do(b, as)
 	a = 5 * 4 + 3 / (2 + 3)
 	fmt.Println(a)
 
-	b := 12 * 23 + 1234 - (12 * 23)
+	b = 12 * 23 + 1234 - (12 * 23)
 	fmt.Println(b)
 
-	b++
+	if b > 0 {
+		b++
+	}
+
 	c := 123.123
+	test.Test(a, c, b)
 }
