1fb9d7e30af1741da4b96a21b9e6bac37b35b3fd
enabokov <edwardnabokov@gmail.com>
2019-04-01 13:57:28 +0300

    extend examples

--- examples/test.go
+++ examples/test.go
@@ -1,10 +1,15 @@
-# package main
+package main
 
-#import "fmt"
+import "fmt"
+import "bla"
+import "test"
 
-def nameFunction(a, b) {
-	defer bla.Do(b, as)
-	a := 5 * 4
+
+def nameFunction(a, b, c, d, f, s) {
+	# this is just a comment
+	var a float
+	bla.Do(b, as)
+	a = 5 * 4 + 3 / (2 + 3)
 	fmt.Println(a)
 
 	b := 12 * 23 + 1234 - (12 * 23)
