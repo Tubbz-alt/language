076cca9b730b8e146de0573f0163a325ead996fb
enabokov <edwardnabokov@gmail.com>
2019-04-01 16:44:49 +0300

    rewrite testing example

--- examples/test.go
+++ examples/test.go
@@ -1,26 +1,29 @@
 package main
 
 import "fmt"
-import "bla"
-import "test"
+import "testing"
 
 
 def nameFunction(a, b) {
 	# this is just a comment
 	var a float
-	bla.Do(b, as)
-	a = 5 * 4 + 3 / (2 + 3)
+	var b uint64
+	var c double
+
+	testing.Compare(a, b)
+
+	a = 5 * 4 + 3
 	fmt.Println(a)
 
 	c = 12 * 23 + 1234 - (12 * 23)
 	fmt.Println(b)
 
-	b = x + 1
+	b = 41 + 1 / (12 * 23)
 
 	if b > 0 {
 		b += 2
 	}
 
 	c := 123.123
-	test.Test(a, c, b)
+	testing.Compare(a, c, b)
 }
