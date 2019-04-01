48340214834091aef1a357feb30466726b5a1833
enabokov <edwardnabokov@gmail.com>
2019-04-02 00:12:13 +0300

    initial semantics

---
+++ semantics/semantics.go
@@ -0,0 +1,13 @@
+package semantics
+
+import (
+	"fmt"
+
+	"github.com/enabokov/language/syntax"
+	"github.com/kr/pretty"
+)
+
+func Analyze(ast syntax.TokenProgram) error {
+	fmt.Printf("%# v", pretty.Formatter(ast))
+	return nil
+}
